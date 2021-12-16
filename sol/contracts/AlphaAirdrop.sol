a026f7bc13957e3f2240a529d58d7abf13fea647
coinop-logan <coinop.logan@gmail.com>
2021-12-16 12:47:59 +0000

    remove test from name, and remove test drop

--- sol/contracts/AlphaAirdrop.sol
+++ sol/contracts/AlphaAirdrop.sol
@@ -8,7 +8,7 @@ contract AlphaNymAirdropSet is ERC1155, Ownable
     bool public mintingClosed = false;
     
     // OpenSea's shitty nonstandard ERC1155 support requires this to have a name displayed for the set.
-    string public constant name = "Alpha Nyms Batch Drop Test";
+    string public constant name = "Alpha Nyms";
 
     constructor() 
         ERC1155("https://etc.foundrydao.com/nym_metadata.php?id={id}") 
@@ -36,18 +36,6 @@ contract AlphaNymAirdropSet is ERC1155, Ownable
         onlyOwner
     {
         _setURI(_uri);
-    }
-}
-
-contract dropTest {
-    constructor(AlphaNymAirdropSet airdropContract) {
-
-airdropContract.mint(0x0000000000000000000000000000000000000001, uint256(0x5d7e15ae1b6a6c71a52f6f3257344005bd15ee8bae91aedb67fa7db6946db66b));
-airdropContract.mint(0x0000000000000000000000000000000000000001, uint256(0xb05c968cc2fee4ca4cd3d2ca6e0d9c1d976dcdfb37652097d4aac78dad68367f));
-airdropContract.mint(0x0000000000000000000000000000000000000001, uint256(0x2272293d19a422f90474fbf2a28f0961208c7683081c77715f0b12ac3555ccfd));
-airdropContract.mint(0x0000000000000000000000000000000000000001, uint256(0xbdcacfe00c5ce4b1e715c15d239e1126ac48069280f9fe41f30338627c132ddf));
-airdropContract.mint(0x0000000000000000000000000000000000000001, uint256(0x77ffadf549329f88b0fbdb3e70d5a83e6d74225fe17060738ef1d2c4070b9d2f));
-
     }
 }
 
