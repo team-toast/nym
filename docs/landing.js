af48a5f2ab642e95d6cdd0b6451198a9b85a5f3b
coinop-logan <coinop.logan@gmail.com>
2021-12-16 14:49:31 +0000

    build update

--- docs/landing.js
+++ docs/landing.js
@@ -23900,7 +23900,7 @@ var $author$project$Demos$Landing$View$body = F2(
 							]),
 							_List_fromArray(
 							[
-								$author$project$Demos$Landing$Theme$normalText('One Alpha Nym each was airdropped on 2021.12.15 to any address that ever held FRY, the token for '),
+								$author$project$Demos$Landing$Theme$normalText('One Alpha Nym each was airdropped on 2021.12.16 to any address that ever held FRY, the token for '),
 								$author$project$Demos$Landing$Theme$newTabLink(
 								{text: 'the Foundry project', url: 'https://foundrydao.com/'}),
 								$author$project$Demos$Landing$Theme$normalText(', as of Ethereum block 13727946 (2021.12.2). This set, as with all future Nym sets, are generated randomly (technically, using block headers as an input). Combine this with the huge possibility space of the set, and you can be sure that no one will ever get a Nym that looks quite like another.')
